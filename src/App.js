4d0d7898b09ea426fcedae70abe35c2df0eb2c1a
Amorem <dev@amorem.com>
2021-03-28 10:49:45 +0200

    first query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const queryInfo = useQuery("pokemon", () =>
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.data.results)
+  );
+  console.log("queryInfo", queryInfo);
+  return (
+    <div>
+      {queryInfo.data?.map((result) => (
+        <div key={result.name}>{result.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
