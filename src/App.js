5681b715f844b8f777d874f760487d2bad002d9a
Amorem <dev@amorem.com>
2021-03-28 11:30:23 +0200

    isError

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import axios from "axios";
 function App() {
   const queryInfo = useQuery("pokemon", async () => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
+    // if (true) {
+    //   throw new Error("Test Error");
+    // }
     return axios
       .get("https://pokeapi.co/api/v2/pokemon")
       .then((res) => res.data.results);
   });
   console.log("queryInfo", queryInfo);
-  return queryInfo.isLoading ? (
-    "Loading ..."
-  ) : (
-    <div>
-      {queryInfo.data?.map((result) => (
-        <div key={result.name}>{result.name}</div>
-      ))}
-    </div>
-  );
+  if (queryInfo.isLoading) {
+    return <div>"Loading ..." </div>;
+  }
+  if (queryInfo.isError) {
+    return <div>{queryInfo.error.message}</div>;
+  }
+  if (queryInfo.isSuccess) {
+    return (
+      <div>
+        {queryInfo.data?.map((result) => (
+          <div key={result.name}>{result.name}</div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
